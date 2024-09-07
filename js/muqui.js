1872a3ea1c6d99c3ccc3b71d3c11aa02d6db0d5c
Sofia Morales <sofimoralesroa92@gmail.com>
2024-09-08 00:01:30 +0200

    movimiento de muqui

--- js/muqui.js
+++ js/muqui.js
@@ -1,30 +1,57 @@
 class Muqui {
+  constructor() {
+    // .todos los muquis se crearan con estos valores
+    this.x = 450;
+    this.y = 300;
+    this.h = 120;
+    this.w = 90;
+    this.speed = 20;
 
-    constructor() {
-  
-      // .todos los muquis se crearan con estos valores
-      this.x = 450;
-      this.y = 300;
-      this.h = 120;
-      this.w = 90;
-      this.gravitySpeed = 2;
+    // al crear el muqui:
+
+    // 1. añadir el muqui al DOM
+    this.nodeMuqui = document.createElement("img");
+    this.nodeMuqui.src = "./imagenes/muqui.png";
+    gameBoxNode.append(this.nodeMuqui);
+
+    // 2. ajustamos sus dimensiones y posiciones
+    this.nodeMuqui.style.width = `${this.w}px`;
+    this.nodeMuqui.style.height = `${this.h}px`;
+    this.nodeMuqui.style.position = "absolute";
+    this.nodeMuqui.style.top = `${this.y}px`;
+    this.nodeMuqui.style.left = `${this.x}px`;
+
+    window.addEventListener("keydown", (event) => {
+      if (event.key === "ArrowRight") {
+        // console.log("moviendo a la derecha")
+        this.muquiMovimiento("right");
+      } else if (event.key === "ArrowLeft") {
+        // console.log("moviendo a la izquierda")
+        this.muquiMovimiento("left");
+      } else if (event.key === "ArrowUp") {
+        this.muquiMovimiento("up");
+      } else if (event.key === "ArrowDown"){
+        this.muquiMovimiento("down")
+      }
       
-  
-  
-      // al crear el muqui:
-  
-      // 1. añadir el muqui al DOM
-      this.nodeMuqui = document.createElement("img")
-      this.nodeMuqui.src = "./imagenes/muqui.png" 
-      gameBoxNode.append(this.nodeMuqui)
-  
-      // 2. ajustamos sus dimensiones y posiciones
-      this.nodeMuqui.style.width = `${this.w}px`
-      this.nodeMuqui.style.height = `${this.h}px`
-      this.nodeMuqui.style.position = "absolute" 
-      this.nodeMuqui.style.top = `${this.y}px`
-      this.nodeMuqui.style.left = `${this.x}px`
-  
-  
+
+    });
+  }
+
+  muquiMovimiento(direccion) {
+    if (direccion === "right") {
+      this.x += this.speed; // left es la posicion en X left + es ir a la derecha - es ir a la izquierda
+      this.nodeMuqui.style.left = `${this.x}px`;
+    } else if (direccion === "left") {
+      this.x -= this.speed;
+      this.nodeMuqui.style.left = `${this.x}px`;
+    } else if (direccion === "up") {
+        this.y -= this.speed;
+        this.nodeMuqui.style.top = `${this.y}px`
+    } else if (direccion === "down") {
+        this.y += this.speed;
+        this.nodeMuqui.style.top = `${this.y}px`
     }
+  }
+
 }
