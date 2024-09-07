6e5dc44f270b997171421b0ce73856f2b4e03181
Sofia Morales <sofimoralesroa92@gmail.com>
2024-09-07 21:50:20 +0200

    planificación

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// aca se une todo
+
+// pantallas
+const splashScreenNode = document.querySelector("#splash-screen")
+const gameScreenNode = document.querySelector("#game-screen")
+const gameOverScreenNode = document.querySelector("#game-over-screen")
+
+// boton 
+const startBtnNode = document.querySelector("#start-btn")
+
+// game box
+const gameBoxNode = document.querySelector("#game-box")
+
+
+// variables del juego
+// todas las variables que van a usarse en el juego 
+
+// *Funciones globales del juego
+// 1. Cambiar las pantallas de juego (pasar de pandalla de inicio a pantalla de juego,
+ //cambiar pantalla de juego a pantalla de game over y ir a un nuevo mundo si gana)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /* 
+ Planificación 
+  1. tiene que pretar el boton estar y cambiar de pantalla de inicio a pantalla de juego 
+  2. tiene que aparecer el mundo espacial con muqui y la nave
+  3. tienen que aparecer los meteoritos aleatorios en el espacio
+  4. colision de muqui con los meteoritos.
+  5. muqui se mueve en todas las dimesiones del espacio y evita los meteoritos.
+  6. muqui es interceptado por los meteoritos y pierde 1 vida.
+  7. muqui tiene 3 vidas o vuelve a iniciar el juego.(pantalla de fin de juego)
+  8. muqui llega a la nave espacial y pasa al otro mundo.
+ */
+
+
+  
